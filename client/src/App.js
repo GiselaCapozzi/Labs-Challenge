b86aa66e178ab572b45a24f2fcc7fa2a266ae313
GiselaCapozzi <gisecapozzi@gmail.com>
2020-10-10 16:34:52 -0300

    Searchbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Routes from './Routes';
 import { Route } from 'react-router-dom';
 import SearchBar from '../src/components/SearchBar';
 
-function App({onSearch}) {
+function App() {
+
+const [query, setQuery] = useState('');
+const [contador, setContador] = useState(0);
+const [productos, setProductos] = useState([]);
+const [inicio, setInicio] = useState(0);
+const [pagina, setPagina] = useState(0);
+const limite = 3;
+
+const onSearch = (search) => {
+    setQuery(search);
+    console.log(search);
+    fetch(`http://localhost:4000/api/search?q=${search}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setContador(Math.ceil(data.results.length / limite));
+            setProductos(data.results.slice(inicio, inicio + limite));
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+useEffect(() => {
+  onSearch('vino')
+}, []);
+
+const MayMen = productos.sort((a, b) => {
+  return a.price - b.price;
+});
+
+const handleChange = (e, value) => {
+  var select = value * limite;
+  setPagina(value);
+  if (select >= 50) {
+    select = 0;
+  }
+  setInicio(select);
+  onSearch(query);
+}
+
   return (
     <div className="App">
       <Routes />
--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import './searchBar.css';
 import meli from '../../meli.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons'
 import { useHistory } from 'react-router';
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
     const [category, setCategory] = useState('');
     const history = useHistory();
 
@@ -21,58 +21,42 @@ const SearchBar = () => {
     }
 
     return (
-        <div className = "rectangulo">
-            <div className = "imagen">
-                <img 
-                    className = "mercadoLibre"
-                    src = {meli}
-                    alt = "Mercado Libre"
+        <div className="col-12 rectangulo">
+            <div className="imagen">
+                <img
+                    className="mercadoLibre"
+                    src={meli}
+                    alt="Mercado Libre"
+                    onClick = {() => history.push("/")}
                 />
             </div>
-            <form
-                onSubmit = {onSubmit}
-                className = "searchBar"
-            >
-            <input
-                className = "input"
-                type = "text"
-                name = "input"
-                placeholder = "Buscar ..."
-                id = "buscador"
-                value = {category}
-                onChange = {onChange}
-            />
-            <button
-                className= "presionar"
-                style = {{fontSize: "14px", color: "black"}}
-                type = "submit"
-            >
-                <FontAwesomeIcon icon = {faSearch}/>
-            </button>
-            </form>
+
+            <div className="formulario">
+                <FontAwesomeIcon className = "menu" icon = {faBars} />
+                <form
+                    onSubmit={onSubmit}
+                    className="searchBar"
+                >
+                    <input
+                        className="input"
+                        type="text"
+                        name="input"
+                        placeholder="Buscar ..."
+                        id="buscador"
+                        value={category}
+                        onChange={onChange}
+                    />
+                    <button
+                        className="presionar"
+                        style={{ fontSize: "14px", color: "black" }}
+                        type="submit"
+                    >
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
 
 export default SearchBar;
-
-const [query, setQuery] = useState('');
-const [contador, setContador] = useState(0);
-const [productos, setProductos] = useState([]);
-const [inicio, setInicio] = useState(0);
-const limite = 3;
-
-export const onSearch = (search) => {
-    setQuery(search);
-    console.log(search);
-    fetch(`http://localhost:4000/api/search?q=${search}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            setContador(Math.ceil(data.results.length / limite));
-            setProductos(data.results.slice(inicio, inicio + limite));
-        })
-        .catch(err => {
-            console.log(err);
-        });
-};
